64aab8e03910583c4c11a45498bc3de16baa775a
Stanislav Dyagilev <su-27sm1@rambler.ru>
2022-10-28 16:20:25 +0300

    Modify .eslint.yml Makefile bin/brain-games.js src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const askUsersName = () => {
+const askUsersName = () => {
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
 };
+export default askUsersName;
