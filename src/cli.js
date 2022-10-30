dac7fea7985650efe9e0f2ccc74b15f26fa3c29d
Stanislav Dyagilev <su-27sm1@rambler.ru>
2022-10-30 21:00:36 +0300

    Delete src/cli.js src/even.js

--- src/cli.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const askUsersName = () => {
-  const name = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${name}!`);
-};
-export default askUsersName;
