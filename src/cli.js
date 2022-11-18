8848401e114971e369ab8296549f809d919e988a
Stanislav Dyagilev <su-27sm1@rambler.ru>
2022-11-18 16:40:23 +0300

    Modify src/cli.js
    cancel deletion function name and deletion import readlineSync

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,10 @@
+import readlineSync from 'readline-sync';
+
 const grettings = () => {
   console.log('Welcome to the Brain Games!');
+
+  const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
 };
 
 export default grettings;
