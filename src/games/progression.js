6248df2c8b8137ba9364761ddd37d83f520f97db
Stanislav Dyagilev <su-27sm1@rambler.ru>
2022-11-19 21:58:48 +0300

    Modify src/bin/progression.js
    Renaming functions mainPlayGame and roundGenerateGame
    Changing import export path of function mainPlayGame
    Adding const gameDescription

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,38 +1,23 @@
-import readlineSync from 'readline-sync';
+import mainPlayGame from '../index.js';
 
-import askUsersName from '../index.js';
 import getRandomInt from '../utils.js';
 
-const usersName = askUsersName();
+const gameDescription = 'What number is missing in the progression?';
+const roundGenerateGame = () => {
+  let startNumber = getRandomInt(1, 100);
+  const multiplier = getRandomInt(2, 10);
 
-const playProgressionGame = () => {
-  console.log('What number is missing in the progression?');
+  let i = 0;
+  const arr = [];
 
-  for (let c = 0; c < 3; c += 1) {
-    let startNumber = getRandomInt(1, 100);
-    const multiplier = getRandomInt(2, 10);
-
-    let i = 0;
-    const arr = [];
-
-    while (i < getRandomInt(5, 11)) {
-      startNumber += multiplier;
-      i += 1;
-      arr.push(startNumber);
-    }
-    const rightAnswer = arr.splice(getRandomInt(2, 4), 1, '..').join('');
-    const newArr = arr.join(' ').split(',');
-    console.log(`Question: ${newArr}`);
-    const usersAnswer = readlineSync.question('Your answer: ');
-
-    if (usersAnswer === rightAnswer) {
-      console.log('Correct!');
-    } else {
-      const uncorrectMessage = `${usersAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.\nLet's try again, ${usersName}!`;
-      console.log(uncorrectMessage);
-      return;
-    }
+  while (i < getRandomInt(5, 11)) {
+    startNumber += multiplier;
+    i += 1;
+    arr.push(startNumber);
   }
-  console.log(`Congratulations, ${usersName}!`);
+  const correctAnswer = arr.splice(getRandomInt(2, 4), 1, '..').join('');
+  const newArr = arr.join(' ').split(',');
+  const question = `${newArr}`;
+  return [question, correctAnswer];
 };
-export default playProgressionGame;
+export default mainPlayGame(gameDescription, roundGenerateGame);
