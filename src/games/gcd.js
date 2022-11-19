6cab60c181bc2ccdb34217f97659d86c0a11cfe5
Stanislav Dyagilev <su-27sm1@rambler.ru>
2022-11-19 21:16:35 +0300

    Modify src/games/gcd.js
    Changing import export path function mainPlayGame
    Rename function roundGenerateGame and optimization code
    Adding const gameDescription

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,45 +1,33 @@
-import readlineSync from 'readline-sync';
+import mainPlayGame from '../index.js';
 
-import askUsersName from '../index.js';
 import getRandomInt from '../utils.js';
 
-const usersName = askUsersName();
+const gameDescription = 'Find the greatest common divisor of given numbers.';
+const roundGenerateGame = () => {
+  const num1 = getRandomInt(1, 100);
+  const num2 = getRandomInt(1, 100);
 
-const playDivisorGame = () => {
-  console.log('Find the greatest common divisor of given numbers.');
+  const divNum1 = [];
+  const divNum2 = [];
 
-  for (let c = 0; c < 3; c += 1) {
-    const num1 = getRandomInt(1, 100);
-    const num2 = getRandomInt(1, 100);
-
-    const divNum1 = [];
-    const divNum2 = [];
-
-    for (let i = num1; i > 0; i -= 1) {
-      if (num1 % i === 0) {
-        divNum1.push(i);
-      }
-    }
-
-    for (let j = num2; j > 0; j -= 1) {
-      if (num2 % j === 0) {
-        divNum2.push(j);
-      }
-    }
-
-    const intersection = divNum1.filter((x) => divNum2.includes(x));
-    console.log(`Question: ${num1} ${num2}`);
-    const usersAnswer = Number(readlineSync.question('Your answer: '));
-
-    if (usersAnswer === intersection[0]) {
-      console.log('Correct!');
-    } else {
-      const uncorrectMessage = `${usersAnswer} is wrong answer ;(. Correct answer was ${intersection[0]}.\nLet's try again, ${usersName}!`;
-      console.log(uncorrectMessage);
-      return;
+  for (let i = num1; i > 0; i -= 1) {
+    if (num1 % i === 0) {
+      divNum1.push(i);
     }
   }
 
-  console.log(`Congratulations, ${usersName}!`);
+  for (let j = num2; j > 0; j -= 1) {
+    if (num2 % j === 0) {
+      divNum2.push(j);
+    }
+  }
+
+  const intersection = divNum1.filter((x) => divNum2.includes(x));
+  const question = `${num1} ${num2}`;
+  const correctAnswer = String(intersection[0]);
+  return [question, correctAnswer];
 };
-export default playDivisorGame;
+
+export default () => {
+  mainPlayGame(gameDescription, roundGenerateGame);
+};
