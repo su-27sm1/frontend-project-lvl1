e8fb6e68fd8e0d736a44fb32c658c26dc91fd7b8
Stanislav Dyagilev <su-27sm1@rambler.ru>
2022-11-19 21:44:04 +0300

    Modify src/games/prime.js
    Renaming functions roundGenerateGame and mainPlayGame
    Updating import export path of function mainPlayGame
    Optimization code by transit it to index.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,42 +1,25 @@
-import readlineSync from 'readline-sync';
+import mainPlayGame from '../index.js';
 
-import askUsersName from '../index.js';
 import getRandomInt from '../utils.js';
 
-const usersName = askUsersName();
+// eslint-disable-next-line operator-linebreak
+const gameDescription =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
+const roundGenerateGame = () => {
+  const num = getRandomInt(1, 100);
 
-const playPrimeGame = () => {
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  const question = num;
 
-  for (let c = 0; c < 3; c += 1) {
-    const num = getRandomInt(1, 100);
-
-    console.log(`Question: ${num}`);
-    const usersAnswer = readlineSync.question('Your answer: ');
-
-    const isPrime = () => {
-      for (let i = 2; i <= num / 2; i += 1) {
-        if (num % i === 0) {
-          return false;
-        }
+  const isPrime = () => {
+    for (let i = 2; i <= num / 2; i += 1) {
+      if (num % i === 0) {
+        return 'no';
       }
-      return true;
-    };
-    isPrime(num);
-
-    const meaning1 = isPrime() === true && usersAnswer === 'yes';
-    const meaning2 = isPrime() === false && usersAnswer === 'no';
-    if (meaning1 || meaning2) {
-      console.log('Correct!');
-    } else {
-      const rightAnswer = usersAnswer === 'yes' ? 'no' : 'yes';
-
-      const uncorrectMessage = `${usersAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.\nLet's try again, ${usersName}!`;
-      console.log(uncorrectMessage);
-      return;
     }
-  }
-  console.log(`Congratulations, ${usersName}!`);
+    return 'yes';
+  };
+  const correctAnswer = isPrime(num);
+  return [question, correctAnswer];
 };
 
-export default playPrimeGame;
+export default mainPlayGame(gameDescription, roundGenerateGame);
