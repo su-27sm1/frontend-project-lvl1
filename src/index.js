0420970446e7d240d933ff9a678334f53c3201fe
Stanislav Dyagilev <su-27sm1@rambler.ru>
2022-11-19 12:53:12 +0300

    Modify src/index.js
    Changing general logic of all file. Changing import export paths and namesfunctions
    Creating new loop and function with general games logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import readlineSync from 'readline-sync';
 
-const askUsersName = () => {
+const roundsCount = 3;
+const mainPlayGame = (gameDescription, roundGenerateGame) => {
   console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(gameDescription);
 
-  const name = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${name}!`);
-  return name;
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [question, correctAnswer] = roundGenerateGame();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      return console.log(
+        // eslint-disable-next-line comma-dangle
+        `${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`
+      );
+    }
+  }
+  return console.log(`Congratulations, ${userName}!`);
 };
 
-export default askUsersName;
+export default mainPlayGame;
