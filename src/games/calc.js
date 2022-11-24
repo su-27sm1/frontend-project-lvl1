f77e63db61e0b454ab08a183465237d397125c91
Stanislav Dyagilev <su-27sm1@rambler.ru>
2022-11-24 11:23:46 +0300

    Modify src/games/calc.js
    Renaming function genRandOperator => getRandOperator
    Renaming const arr => operators
    Optimization code by deletion const rand by adding return operators

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,10 +1,9 @@
 import runGame from '../index.js';
 import getRandomInt from '../utils.js';
 
-const genRandOperator = () => {
-  const arr = ['+', '-', '*'];
-  const rand = () => getRandomInt(0, 2);
-  return arr[rand()];
+const getRandOperator = () => {
+  const operators = ['+', '-', '*'];
+  return operators[getRandomInt(0, 2)];
 };
 
 const gameDescription = 'What is the result of the expression?';
@@ -12,7 +11,7 @@ const gameDescription = 'What is the result of the expression?';
 const makeRound = () => {
   const num1 = getRandomInt(1, 100);
   const num2 = getRandomInt(1, 100);
-  const operator = genRandOperator();
+  const operator = getRandOperator();
   const question = `${num1} ${operator} ${num2}`;
   let result = 0;
 
