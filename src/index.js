0a66aee2bed0f6aa3bf7d210009f2f0759c4f2a3
Stanislav Dyagilev <su-27sm1@rambler.ru>
2022-11-24 10:33:14 +0300

    Modify src/index.js
    Renaming functions makeRound and runGame
    Adding line break between roundsCount and runGame for fit reading and better view

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const roundsCount = 3;
+
 const mainPlayGame = (gameDescription, roundGenerateGame) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
