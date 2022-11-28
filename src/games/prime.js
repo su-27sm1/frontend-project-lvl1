b727b55db818200ef59efcec62cf22e23139e697
Stanislav Dyagilev <su-27sm1@rambler.ru>
2022-11-28 21:44:15 +0300

    Modify src/games/prime.js
    Transition function isPrime out of makeRound
    Changing return meaning of isPrime to boolean
    Improving logic of const correctAnswer

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,21 +5,19 @@ import getRandomInt from '../utils.js';
 const gameDescription =
   'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+const isPrime = (num) => {
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const makeRound = () => {
   const num = getRandomInt(1, 100);
-
   const question = num;
-
-  const isPrime = () => {
-    for (let i = 2; i <= num / 2; i += 1) {
-      if (num % i === 0) {
-        return 'no';
-      }
-    }
-    return 'yes';
-  };
-
-  const correctAnswer = isPrime(num);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
