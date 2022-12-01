d5f48c7eb5f314cb1ae94de663e574fe6ce1aaa0
Stanislav Dyagilev <su-27sm1@rambler.ru>
2022-12-01 17:50:38 +0300

    Modify src/games/calc.js
    Improving function getOperatorExpression adding return instead of let result
    Pulling getOperatorExpression out of makeRound
    Updating getRandOperator return operators[getRandomInt(0, operators.length)] instead of (0, 2)
    Adding default: throw new Error()

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,7 +3,20 @@ import getRandomInt from '../utils.js';
 
 const getRandOperator = () => {
   const operators = ['+', '-', '*'];
-  return operators[getRandomInt(0, 2)];
+  return operators[getRandomInt(0, operators.length)];
+};
+
+const getOperatorExpression = (num1, operator, num2) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      throw new Error(`Invalid operator - ${operator}`);
+  }
 };
 
 const gameDescription = 'What is the result of the expression?';
@@ -12,26 +25,8 @@ const makeRound = () => {
   const num1 = getRandomInt(1, 100);
   const num2 = getRandomInt(1, 100);
   const operator = getRandOperator();
-
-  const getOperatorExpression = () => {
-    let result = 0;
-    switch (operator) {
-      case '+':
-        result = num1 + num2;
-        break;
-      case '-':
-        result = num1 - num2;
-        break;
-      default:
-        result = num1 * num2;
-        break;
-    }
-    return String(result);
-  };
-
   const question = `${num1} ${operator} ${num2}`;
-  const correctAnswer = getOperatorExpression(num1, num2, operator);
-
+  const correctAnswer = String(getOperatorExpression(num1, operator, num2));
   return [question, correctAnswer];
 };
 
